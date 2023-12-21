5acac66fe7a47ad3e0c252ef9a2894c591a7b12e
nilesh-bhardwaj <nilesh-bhardwaj@csgroupchd.com>
2023-12-21 15:11:24 +0530

    changes

---
+++ public/js/addproduct.js
@@ -0,0 +1,3 @@
+setTimeout(function () {
+    document.getElementById("loading").classList.add("none");
+}, 3000);
---
+++ resources/views/addproduct.blade.php
@@ -0,0 +1,57 @@
+<x-app-layout>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+            crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+        <link href="{{ asset('css/payment.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/product.css') }}" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <style>
+            .icon-minus:before {
+                content: "\f068";
+            }
+
+            .icon-plus:before {
+                content: "\f067";
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="spinner-wrapper" id="loading">
+            <div class="spinner"></div>
+            <div class="spinner">
+                <div class="rect1"></div>
+                <div class="rect2"></div>
+                <div class="rect3"></div>
+                <div class="rect4"></div>
+                <div class="rect5"></div>
+            </div>
+        </div>
+
+        <div id="header">
+            <h1>Payments Details</h1>
+        </div>
+
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900 dark:text-gray-100">
+               
+        </div>
+        </div>
+    </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="{{ asset('js/addproduct.js') }}"></script>
+
+    </html>
+</x-app-layout>
